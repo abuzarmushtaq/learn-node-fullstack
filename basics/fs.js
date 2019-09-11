0757a8e0a4c9862371e3cc3082549d2a0a388913
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2019-09-11 16:58:08 +0500

    fs complete

--- basics/fs.js
+++ basics/fs.js
@@ -1,27 +1,59 @@
 let fs = require('fs');
 // let http = require('http');
 
+
 /*
-Read files
-Create files
-Update files
+Create/Update files
+*/
+
+// fs.writeFile('dummy.txt', 'Hello World!', (err, file) => {
+//     if (err) throw err;
+//     console.log('Saved');
+// });
+
+
+/*
+Append data into files
+*/
+
+// fs.appendFile('dummy.txt', ' This is new next!', (err) => {
+//     if (err) throw err;
+//     console.log('Updated!');
+// });
+
+
+/*
 Delete files
+*/
+
+// fs.unlink('dummy.txt',  (err) => {
+//     if (err) throw err;
+//     console.log('File deleted');
+// });
+
+
+/*
 Rename files
 */
 
-
+// fs.rename('dummy.txt', 'demo.txt', (err) => {
+//     if (err) throw err;
+//     console.log('File Renamed');
+// });
 
 
 
 
 /*
-let server = http.createServer((req, res) => {
-    fs.readFile('index.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    });
-})
-
-server.listen(8080);
+ Simple Server
 */
+
+// let server = http.createServer((req, res) => {
+//     fs.readFile('index.html', function (err, data) {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data);
+//         res.end();
+//     });
+// })
+
+// server.listen(8080);
