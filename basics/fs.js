5029c04f9458d9d0ff3cc53d3ab4348f35c7d6b5
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2019-09-11 16:37:56 +0500

    fs

---
+++ basics/fs.js
@@ -0,0 +1,27 @@
+let fs = require('fs');
+// let http = require('http');
+
+/*
+Read files
+Create files
+Update files
+Delete files
+Rename files
+*/
+
+
+
+
+
+
+/*
+let server = http.createServer((req, res) => {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
+})
+
+server.listen(8080);
+*/
