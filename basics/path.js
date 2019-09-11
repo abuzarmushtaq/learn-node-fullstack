1d1de004a0e7f288b4a49e4195a866ea03aa522e
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2019-09-11 17:10:25 +0500

    path

---
+++ basics/path.js
@@ -0,0 +1,33 @@
+let path = require('path');
+
+// __dirname
+// __filename
+
+
+/*
+Returns the directories of a path
+*/
+
+// console.log(path.dirname(__dirname));
+
+
+/*
+Joins the specified paths into one
+*/
+
+// console.log(path.join('dummyuser', 'project', 'dummy.txt'));
+
+
+/*
+Returns the last part of a path
+*/
+
+// console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+
+
+/*
+Returns the file extension of a path
+*/
+
+// console.log(path.extname(__filename))
