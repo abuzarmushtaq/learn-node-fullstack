4467f6562698de5ad95bd706a0fe1fab6df0a046
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2019-09-17 18:57:40 +0500

    url update

--- basics/url.js
+++ basics/url.js
@@ -1,7 +1,7 @@
 let url = require('url');
 
-var adress = 'http://localhost:8080/user.html?username=test10&dateofbirth=1998 february';
-var q = url.parse(adress, true);
+var address = 'http://localhost:8080/user.html?username=test10&dateofbirth=1998 february';
+var q = url.parse(address, true);
 
 console.log(q.host); // returns 'localhost:8080'
 console.log(q.pathname); // returns '/user.html'
