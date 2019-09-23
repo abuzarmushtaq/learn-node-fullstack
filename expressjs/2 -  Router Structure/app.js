846d504f9bca8213109b0a679a2ce625929bbd54
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2019-09-23 17:16:22 +0500

    add helper comments

--- expressjs/2 -  Router Structure/app.js
+++ expressjs/2 -  Router Structure/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Seperate code into modules aka files
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -12,6 +13,7 @@ app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
+    // .status(404) function for handling 404 req
     res.status(404).send('<h1>Page not found</h1>');
 })
 
